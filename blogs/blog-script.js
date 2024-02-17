226177ac3d563170c91763bc7f0c4047d3619eeb
Nader El-Ghotmi <43392069+NaderEG@users.noreply.github.com>
2024-02-17 18:47:41 -0500

    resume and blog

---
+++ blogs/blog-script.js
@@ -0,0 +1,5 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    // JavaScript code to enhance the blog functionality
+    console.log('Page loaded. Enhance as needed.');
+});
